dbc4f75ac4b78631a083a7f6065e8cb4f64cec1b
nayan-21 <githubnayan57@gmail.com>
2025-06-11 15:39:56 +0530

    proxy server setup

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,7 @@
-export const LOGO = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const LOGO =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const MENU = "http://localhost:5000/api/menu/";
+export const MENU = "https://food-zone-backend-hz7e.onrender.com/api/menu/";
 
-
-export const RESTAURANT = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const RESTAURANT =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
